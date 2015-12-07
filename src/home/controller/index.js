85f5d0a94de4f6e637bcd1ce16b6b6f4c711a6e3
andy <Baichao Lingbochao.ling@qianmo-inc.com>
2015-12-07 18:33:14 +0800

    success

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -3,17 +3,22 @@
 import Base from './base.js';
 
 export default class extends Base {
-  /**
-   * index action
-   * @return {Promise} []
-   */
-  indexAction(){
-    //auto render template file index_index.html
-    return this.display();
-  }
+    /**
+    * index action
+    * @return {Promise} []
+    */
+    indexAction(){
+        //auto render template file index_index.html
+        return this.display();
+    }
 
-  aniAction(){
-    //auto render template file index_index.html
-    return this.display();
-  }
+    aniAction(){
+        //auto render template file index_index.html
+        return this.display();
+    }
+
+    successAction(){
+        //auto render template file index_index.html
+        return this.display();
+    }
 }
